a4398d849bf9ea748f269deaf77e985ae4f1cdb7
Talent Antonio <antonio.talenty@gmail.com>
2019-02-20 09:04:38 +0000

    Calculator

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = ({display}) => {
+  return (
+    <div className="calculator-display">
+    {display || 0}
+    {display || 0}
+    </div>
+    ,
+     <div className="calculator-display">
+     {display || 0}
+     {display || 0}
+     </div>
+  );
+}
+
+export default CalculatorDisplay;
